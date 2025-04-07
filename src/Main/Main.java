7e2d7f551d105ef834a9e01d66784197726d6326
JuanJose51 <juanj,orozcoc@uqvirtual.edu.co>
2025-04-06 21:58:45 -0500

    se agragan metodos para agregar vehiculo blindado en la clase mein

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,13 +4,14 @@ import javax.swing.JOptionPane;
 
 import Logica.Batallon;
 import Modelos.EstadoOperativo;
+import Modelos.VehiculoBlindado;
 
 public class Main {
 	public  static Batallon b=new Batallon("los mas machos");
 
 	public static void main(String[] args) {
 		int opcion=0;
-		String mensaje="Ingrese una opción \n"+"1.Gestion de Vehiculos\n"+"2.Gestion de Misiones"+"3.salir";
+		String mensaje="Ingrese una opción \n"+"1.Gestion de Vehiculos\n"+"2.Gestion de Misiones\n"+"3.salir";
 		String gestionMisiones="Ingrese una Opción\n"+"1.Agregar Mision"+"2.buscar mision"+"3.Eliminar Mision"+"4.actualizar"+"5.salir";
 		while(opcion!= 3) {
 			opcion=Integer.parseInt(JOptionPane.showInputDialog(mensaje));
@@ -44,32 +45,35 @@ public class Main {
 		}
 	}
 	public static void agregarVehiculo() {
-		String mensaje ="Ingrese una opcion de tipo de Vehiculo"+"1.blindado\n"+"2.de Apoyo\n"+"3.transporte de tropas";
-		int tipoVehiculo =Integer.parseInt(JOptionPane.showInputDialog(mensaje));
+		int tipoVehiculo =Integer.parseInt(JOptionPane.showInputDialog("ingrese el numero de la opcion\n"+"1.Vehiculo Blindado\n"+"2.Vehiculo de apoyo\n"+"3.Vehiculo de tansporte de tropas"));
 		switch(tipoVehiculo) {
 		case 1:
-			String id=JOptionPane.showInputDialog("Ingrese el id del vehiculo");
-			boolean estado =b.existeVehiculoBlindado(id);
-			if(estado != true) {
-				String modelo=JOptionPane.showInputDialog("Ingrese el modelo del vehiculo");
-				String yearDeFabricacion =JOptionPane.showInputDialog("Ingrese el año de fabricacion del vehiculo");
-				int kilometraje =Integer.parseInt(JOptionPane.showInputDialog("ingrese el en numeros enteros el kilometraje del vehiculo"));
-				EstadoOperativo estadoSeleccionado =(EstadoOperativo)JOptionPane.showInputDialog(null,"Seleccione el estado operativo del vehículo:",
-			            "Estado Operativo",
-			            JOptionPane.QUESTION_MESSAGE,
-			            null,
-			            EstadoOperativo.values(),    
-			            EstadoOperativo.DISPONIBLE 
-			        );
+			EstadoOperativo estadoSeleccionado =(EstadoOperativo)JOptionPane.showInputDialog(null,"Seleccione el estado operativo del vehículo:",
+		            "Estado Operativo",
+		            JOptionPane.QUESTION_MESSAGE,
+		            null,
+		            EstadoOperativo.values(),    
+		            EstadoOperativo.DISPONIBLE 
+		        );
+			boolean estado;
+			String id;
+			do {
+				id=JOptionPane.showInputDialog("ingrese un id para el vehiculo");
+				estado =b.existeVehiculoBlindado(id);
+			}while(estado !=false);
+			String modelo =JOptionPane.showInputDialog("Ingrese el modelo del Vehiculo");
+			String yearDeFabricacion=JOptionPane.showInputDialog("ingrese el año de fabricación");
+			int kilometrage=Integer.parseInt(JOptionPane.showInputDialog("ingrese el kilometrage del vehiculo en numeros enteros"));
+			String nivel =JOptionPane.showInputDialog("ingrese el nivel de blindaje\n"+"bajo\n"+"medio\n"+"alto\n");
+			VehiculoBlindado v=new VehiculoBlindado(id, modelo, yearDeFabricacion, kilometrage, estadoSeleccionado, nivel);
+			boolean estado1 =b.agregarVehiculoBlindado(v);
+			if(estado1 !=false) {
+				JOptionPane.showMessageDialog(null, "Se agrego con exito :)");
+			}else {
+				JOptionPane.showMessageDialog(null, "Error:no se agrego con exito :(");
 			}
 			break;
-		case 2:
-			
-			break;
-		case 3:
-			break;
 		}
-		
 	}
 	
 
